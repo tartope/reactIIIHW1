4460a08265952bd4856168760be5fa6ca14435e3
Tunisia Artope <tartope@xpanxion.com>
2023-05-31 20:15:14 -0400

    Add Repositories component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query-devtools';
+// import { ReactQueryDevtools } from 'react-query-devtools';
+import Repositories from './components/Repositories';
+
 
 const queryClient = new QueryClient({});
 
@@ -10,22 +10,8 @@ function App() {
     <div className="App">
       <QueryClientProvider client={queryClient}>
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-
-        <ReactQueryDevtools initialIsOpen={true} />
+        <Repositories />
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
 
       </QueryClientProvider>
     </div>
---
+++ src/components/Repositories.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+ 
+ 
+ 
+export default function Repositories() {
+    // Fisrt arguement: a unique key called 'repo' to the useQuery hook, second argument: the promise-based function for fetching our data where axios is used
+    // Make use of object destructuring to destructure the object response
+    const { isLoading, isError, data, error, refetch } = useQuery(["repo"], () =>
+        axios
+            .get("https://api.github.com/users/eunit99/repos")
+            .then((res) => res.data)
+    );
+
+    if (isLoading) return "Loading...";
+
+    if (error) return "An error has occurred: " + error.message;
+
+    console.log(data)
+
+    return (
+    <>
+        {data.map(repo=>{
+            return (
+                <Fragment
+                    key={repo.id}
+                >
+                    <ul>
+                        <li>
+                            <a
+                                href={repo.clone_url}>
+                                {repo.name}
+                            </a>
+                        </li>
+                    </ul>
+                </Fragment>
+            )
+        })}
+        <button type="button" onClick={refetch}>
+            Fetch again
+        </button>
+    </>
+    )
+};
